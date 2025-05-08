6743203da61c385d14fd1fd2e6b66b1156d45422
eelizavetochkaa <143887936+eelizavetochkaa@users.noreply.github.com>
2025-05-08 13:42:47 +0300

    домашнее задание

--- homeWork/05/template1/template.hpp
+++ homeWork/05/template1/template.hpp
@@ -2,12 +2,30 @@
 #include <cmath>
 #define EPS 1e-10
 
-
-auto fibonacci() -> T{
+template<int n>
+auto fibonacci() -> int{
+  if constexplr (n==0||n==1) {
+    return n;
+  } else {
+    return fibonacci<n-1>() + fibonacci<n-2>();
+  }
 
 }
 
-
+template<int n>
 class Fibonacci{
+public:
+  static constexpr int value = Fibonacci<n-1>::value + Fibonacci<n-2>::value;
+};
 
+template<>
+class Fibonacci<0> {
+public:
+    static constexpr int value = 0;
+};
+
+template<>
+class Fibonacci<1> {
+public:
+    static constexpr int value = 1;
 };
