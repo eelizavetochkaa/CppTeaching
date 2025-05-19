7acc556c866b9f73406d2b0edcced9471732d53e
eelizavetochkaa <143887936+eelizavetochkaa@users.noreply.github.com>
2025-05-19 05:41:16 +0000

    Богданова Елизавета

--- homeWork/05/template1/main.cpp
+++ homeWork/05/template1/main.cpp
@@ -4,5 +4,6 @@
 using namespace std;
 
 int main(){
+    std::cout << "fibonacci<10>() = " << fibonacci<10>() << "\n";
     return 0;
 }
--- homeWork/05/template1/template.hpp
+++ homeWork/05/template1/template.hpp
@@ -2,12 +2,29 @@
 #include <cmath>
 #define EPS 1e-10
 
-
-auto fibonacci() -> T{
-
+template<int N>
+constexpr auto fibonacci() -> int {
+    if constexpr (N == 0 || N == 1) {
+        return N;
+    } else {
+        return fibonacci<N-1>() + fibonacci<N-2>();
+    }
 }
 
+template<int N>
+class Fibonacci {
+public:
+    static constexpr int value = Fibonacci<N-1>::value + Fibonacci<N-2>::value;
+};
 
-class Fibonacci{
+template<>
+class Fibonacci<0> {
+public:
+    static constexpr int value = 0;
+};
 
+template<>
+class Fibonacci<1> {
+public:
+    static constexpr int value = 1;
 };
